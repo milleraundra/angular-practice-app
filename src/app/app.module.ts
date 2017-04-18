97dd3dc247a585da4058442b4b03003fba73e515
milleraundra <miller.aundra@gmail.com>
2017-04-18 13:21:34 -0700

    create new list item component using Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListitemComponent } from './listitem/listitem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListitemComponent
   ],
   imports: [
     BrowserModule,
