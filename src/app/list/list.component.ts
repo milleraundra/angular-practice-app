92ff3650c9cc69666e8f44574e88f034843f3025
milleraundra <miller.aundra@gmail.com>
2017-04-25 10:49:26 -0700

    update template to allow wrapping use inline css

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,25 @@
 /**
  * Created by aundramiller on 4/18/17.
  */
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-list',
-  template: '<app-listitem></app-listitem><app-listitem></app-listitem>',
-  styleUrls: ['./list.component.css']
+  template: `
+    <ul>
+      <li>
+        <app-listitem></app-listitem>   
+      </li>
+      <li>
+        <app-listitem></app-listitem>   
+      </li>
+    </ul>`,
+  // styleUrls: ['./list.component.css']
+  styles: [`
+    ul {
+      list-style-type: none;
+    }
+  `]
 })
 
 export class ListComponent {
